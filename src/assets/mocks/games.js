a8114e66ee2e07ed497b64dcc9bf9810a813366b
MARTA GARCIA <martagj94@gmail.com>
2023-05-14 16:57:36 +0200

    add content to gameInformation and rulesAndPolicies components

---
+++ src/assets/mocks/games.js
@@ -0,0 +1,167 @@
+export const information = {
+    games: {
+      september: {
+        "9/01": {
+          games: [
+            {
+              team: "U1 and U4",
+              location: "AJ Katzenmaier",
+              time: "9:30 a.m.",
+            },
+            {
+              team: "U3 and U2",
+              location: "Greenbay",
+              time: "1:00 p.m.",
+            },
+          ],
+        },
+        "9/08": {
+          games: [
+            {
+              team: "U5 and U6",
+              location: "Howard A Yeager",
+              time: "9:30 a.m.",
+            },
+            {
+              team: "U6 and U1",
+              location: "Marjorie P Hart",
+              time: "1.00 p.m.",
+            },
+          ],
+        },
+        "9/15": {
+          games: [
+            {
+              team: "U2 and U4",
+              location: "North",
+              time: "9:30 a.m.",
+            },
+            {
+              team: "U3 and U5",
+              location: "AJ Katzenmaier",
+              time: "1.00 p.m.",
+            },
+          ],
+        },
+        "9/22": {
+          games: [
+            {
+              team: "U1 and U3",
+              location: "South",
+              time: "9:30 a.m.",
+            },
+            {
+              team: "U2 and U6",
+              location: " Howard A Yeager",
+              time: "1.00 p.m.",
+            },
+          ],
+        },
+        "9/29": {
+          games: [
+            {
+              team: "U4 and U5",
+              location: "Greenbay",
+              time: "9:30 a.m.",
+            },
+          ],
+        },
+      },
+      october: {
+        "10/06": {
+          games: [
+            {
+              team: "U2 and U5",
+              location: " Marjorie P Hart",
+              time: "9:30 a.m.",
+            },
+            {
+              team: "U1 and U6",
+              location: "South",
+              time: "1:00 p.m.",
+            },
+          ],
+        },
+        "10/13": {
+          games: [
+            {
+              team: "U3 and U4",
+              location: " Howard A Yeager",
+              time: "9:30 a.m.",
+            },
+            {
+              team: "U5 and U1",
+              location: "Greenbay",
+              time: "1:00 p.m.",
+            },
+          ],
+        },
+        "10/20": {
+          games: [
+            {
+              team: "U6 and U3",
+              location: "North",
+              time: "9:30 a.m.",
+            },
+            {
+              team: "U2 and U4",
+              location: "Marjorie P Hart",
+              time: "1:00 p.m.",
+            },
+          ],
+        },
+        "10/27": {
+          games: [
+            {
+              team: "U3 and U1",
+              location: "AJ Katzenmaier",
+              time: "9:30 a.m.",
+            },
+            {
+              team: "U5 and U6",
+              location: "Howard A Yeager",
+              time: "1:00 p.m.",
+            },
+          ],
+        },
+      },
+    },
+    locations: {
+      "AJ Katzenmaier": {
+        fullName: "AJ Katzenmaier Elementary",
+        address: "24 W. Walton St., Chicago, IL 60610",
+        mapUrl:
+          "https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d79896.00036669537!2d-87.63432217110461!3d41.91107217731799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x880fd34e07f6bac3%3A0x68a82e5d59952c86!2s24+W+Walton+St%2C+Chicago%2C+IL+60610!3m2!1d41.9002367!2d-87.6290717!5e0!3m2!1sen!2sus!4v1498661900848",
+      },
+      Greenbay: {
+        fullName: "Greenbay Elementary",
+        address: "1734 N. Orleans St., Chicago, IL 60614",
+        mapUrl:
+          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.0251959124826!2d-87.64023168455779!3d41.91381707921941!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34073f306a3%3A0x9e1726bbf8f23f0e!2s1734+N+Orleans+St%2C+Chicago%2C+IL+60614!5e0!3m2!1sen!2sus!4v1498666206411",
+      },
+      "Howard A Yeager": {
+        fullName: "Howard A Yeager Elementary",
+        address: "2245 N. Southport Ave., Chicago, IL 60614",
+        mapUrl:
+          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.585683085616!2d-87.66511458455746!3d41.9232645792187!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f9b8d2d%3A0x62ad8b907dd755d6!2s2245+N+Southport+Ave%2C+Chicago%2C+IL+60614!5e0!3m2!1sen!2sus!4v1498666257800",
+      },
+      "Marjorie P Hart": {
+        fullName: "Marjorie P Hart Elementary",
+        address: "2625 N. Orchard St., Chicago, IL 60614",
+        mapUrl:
+          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5936.571390936897!2d-87.6478374194755!3d41.92971193428922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd30f2630e551%3A0x3e719e44a5cef714!2s2625+N+Orchard+St%2C+Chicago%2C+IL+60614!5e0!3m2!1sen!2sus!4v1498666301447",
+      },
+      North: {
+        fullName: "North Elementary",
+        address: "1409 N. Ogden Ave., Chicago, IL 60610",
+        mapUrl:
+          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.336427766927!2d-87.648358384558!3d41.90712597921987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd33af13a8945%3A0xb6ad1ec2b6f379ba!2s1409+N+Ogden+Ave%2C+Chicago%2C+IL+60610!5e0!3m2!1sen!2sus!4v1498666346558",
+      },
+      South: {
+        fullName: "South Elementary",
+        address: "2101 N. Fremont St., Chicago, IL 60614",
+        mapUrl:
+          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.749141506375!2d-87.65354528455761!3d41.91975117921891!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3196fb41dc7%3A0x970be7f7d6336df5!2s2101+N+Fremont+St%2C+Chicago%2C+IL+60614!5e0!3m2!1sen!2sus!4v1498666373464",
+      },
+    },
+  };
--- src/components/GameInformation.js
+++ src/components/GameInformation.js
@@ -1,10 +1,89 @@
-import Header from "./Header";
+import React from "react";
+import Template from "./Template";
+import { information } from "./../assets/mocks/games.js";
+import "./../assets/css/gameInformation.css";
 
 const GameInformation = () => {
+  const gamesInfo = information;
+  const months = Object.keys(gamesInfo.games);
+  const locations = Object.keys(gamesInfo.locations);
   return (
     <>
-      <Header />
-      <p>GameInformation</p>
+      <Template title="NYSL Game Information">
+        <h2>Fall Schedule</h2>
+        <p>* All games take place on Saturday</p>
+        {months.map((month, index) => (
+          <table key={month + index} className="table game__table">
+            <thead>
+              <tr>
+                <th scope="col">{month.toUpperCase()}</th>
+                <th scope="col">Teams</th>
+                <th scope="col">Location</th>
+                <th scope="col">Times</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(gamesInfo.games[month]).map((date, index) => (
+                <React.Fragment key={index}>
+                  {gamesInfo.games[month][date].games.map(
+                    (gameDetail, index) => (
+                      <tr key={date + index}>
+                        <th>{index === 0 ? date : ""}</th>
+                        <td>{gameDetail.team}</td>
+                        <td>{gameDetail.location}</td>
+                        <td>{gameDetail.time}</td>
+                      </tr>
+                    )
+                  )}
+                </React.Fragment>
+              ))}
+            </tbody>
+          </table>
+        ))}
+        <p>
+          <strong>Game locations:</strong>
+          {locations.map((location, index) => (
+            <div key={index}>
+              <div className="game__details">
+                <p>
+                  <strong>{gamesInfo.locations[location].fullName}</strong>
+                </p>
+                <p>{gamesInfo.locations[location].address}</p>
+              </div>
+              <iframe
+                className="game__map"
+                src={gamesInfo.locations[location].mapUrl}
+              />
+            </div>
+          ))}
+        </p>
+        <div className="general-info">
+          <p>
+            <strong>Facility Type: </strong>Outdoor
+          </p>
+          <p>
+            <strong>Additional Information: </strong>If deemed necessary by
+            NYSL, games may be shortened or cancelled due to extreme weather
+            conditions. Phone: (630) 690-8132 Email:
+            michael.randall@chisoccer.org
+          </p>
+          <p>
+            <strong>Please direct all questions to: </strong>Michael Randall,
+            League Coordinator
+          </p>
+          <div className="contact-info">
+            <p>
+              <strong>Phone: </strong>(630) 690-8132
+            </p>
+            <p>
+              <strong>Email: </strong>{" "}
+              <a href="mailto:michael.randall@chisoccer.org">
+                michael.randall@chisoccer.org
+              </a>
+            </p>
+          </div>
+        </div>
+      </Template>
     </>
   );
 };
